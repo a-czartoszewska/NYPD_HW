fab4f436a9762cfff9609379bb16c1c80dfc7069
a-czartoszewska <a.czartoszewsk@student.uw.edu.pl>
2025-05-09 11:51:56 +0200

    Update test_nbp.py

--- nbp_course_change/test_nbp.py
+++ nbp_course_change/test_nbp.py
@@ -1,17 +1,16 @@
 import nbp_change
 
-# values, full_name = get_courses(currency_code, days)
+# structure: values, full_name = get_courses(currency_code, days)
 def mock_get_courses(currency_code, days):
+    # mock function for testing
     name = 'Mock name'
     val = [5]*days
-    # for i in range(1, len(days)):
-    #     val.append(val[i-1]+0.2)
     return val, name
 
 def test_calc_statistics(monkeypatch):
     monkeypatch.setattr("nbp_change.get_courses", mock_get_courses)
     res = nbp_change.calc_statistics(["GBP"], 10)
-    # {'GBP': {'change': 1.003759172981944, 'course': 5.0199, 'full_name': 'funt szterling'}}
+    # structure: {'GBP': {'change': 1.003759172981944, 'course': 5.0199, 'full_name': 'funt szterling'}}
     assert res['GBP']['change'] == 1
     assert res['GBP']['course'] == 5
     assert res['GBP']['full_name'] == 'Mock name'
